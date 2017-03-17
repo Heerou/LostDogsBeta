64a5b30c9242b76a03cef95197ac00d45d195e89
Julio Alvarez Pimiento <jalvarezpimiento@gmail.com>
2017-03-16 21:44:03 -0500

    New movement now we don't need the nav mesh

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+	public bool isCollider = false;
+	Vector3 movePosition;
+	public float speed = 10f;
+	public Transform player;
+	
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		RaycastHit hit;
+		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		if(Physics.Raycast(ray, out hit, Mathf.Infinity)) {
+			if(Input.GetMouseButton(0)) {
+				movePosition =  new Vector3(hit.point.x, 0.5f, hit.point.z);
+				transform.position = movePosition;
+				Debug.Log("Mi movimiento es: " + movePosition);
+			}
+		}
+	}
+}
