544ae8f6b7291f7992db7659340f40f7f9fa07b6
Julio Alvarez Pimiento <jalvarezpimiento@gmail.com>
2017-03-26 06:30:33 -0500

    Better movement control

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -4,27 +4,56 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour {
 
-	public bool isCollider = false;
+//Raycast
+	RaycastHit hit;
+	Ray ray;
+
+//Position where i'm going to click	
 	Vector3 movePosition;
 	public float speed = 10f;
-	public Transform player;
+
+	Vector3 initialPosition;
+	
+	Vector3 targetDirection;
+	float angle;
+	Vector3 playerRotation;
+	Coroutine coroutine = null;
+	
 	
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
 	void Update () {
-		RaycastHit hit;
-		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-		if(Physics.Raycast(ray, out hit, Mathf.Infinity)) {
-			if(Input.GetMouseButton(0)) {
-				movePosition =  new Vector3(hit.point.x, 0.5f, hit.point.z);
-				transform.position = movePosition;
-				Debug.Log("Mi movimiento es: " + movePosition);
+		ControlMovement();
+	}
+	void OnTriggerEnter (Collider collider) {
+		if(collider.gameObject.name == "Piedra") {			
+			StopCoroutine(coroutine);
+		}
+	}
+
+	void ControlMovement() {
+		ray = Camera.main.ScreenPointToRay(Input.mousePosition);						
+		//Where the player will go
+		if(Input.GetMouseButton(0)) {
+			if(Physics.Raycast(ray, out hit)) {
+				movePosition =  new Vector3(hit.transform.position.x , 0.5f, hit.transform.position.z);
+				targetDirection = movePosition - transform.position;
+				angle = Vector3.Angle(targetDirection, transform.forward);
+				playerRotation =  transform.eulerAngles;
+				playerRotation.y = angle;
+				transform.eulerAngles = playerRotation;
+				coroutine = StartCoroutine(AwesomeMovement());
 			}
 		}
+	}
+
+	IEnumerator AwesomeMovement() {
+		float timeCounter = 0;
+		initialPosition = transform.position;
+		while (timeCounter < 1) {
+			transform.position = Vector3.Lerp(initialPosition, movePosition, timeCounter);
+			timeCounter += Time.deltaTime;
+			yield return null;
+		}
+		transform.position = movePosition;
 	}
 }
